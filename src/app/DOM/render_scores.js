510d82fd38a8c0d19f220549bf923f9f9c602211
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-27 20:26:50 -0600

    Add event listener to refresh button and add some initiall scores

--- src/app/DOM/render_scores.js
+++ src/app/DOM/render_scores.js
@@ -1,6 +1,7 @@
 export default class ScoresRenderer {
   constructor() {
     this.leaderboard = document.querySelector('.recent-scores-section__scores');
+    this.refreshBtn = document.querySelector('.recent-scores-section__refresh-btn');
   }
 
   loadScore = (score) => {
@@ -9,6 +10,20 @@ export default class ScoresRenderer {
         <p class="recent-scores-section__scores__score__entry">${score.name}: ${score.score}</p>
       </li>
     `);
+  }
+
+  updateLeaderBoard = (latestScores) => {
+    latestScores.forEach((score) => this.loadScore(score));
+  }
+
+  addListener = () => {
+    this.refreshBtn.addEventListener('click', () => {
+      const SAMPLE_SCORES = [ // this is temporary input to populate the leaderboard
+        { name: 'Carlos', score: 70, },
+        { name: 'Andrew', score: 100, },
+      ];
+      this.updateLeaderBoard(SAMPLE_SCORES);
+    });
   }
 }
 
--- src/app/run.js
+++ src/app/run.js
@@ -5,14 +5,8 @@ import loadStaticContent from "./home";
 
 export default function run() {
   loadStaticContent();
-
-  const SAMPLE_SCORES = [
-    { name: 'Carlos', score: 70, },
-    { name: 'Andrew', score: 100, },
-  ];
   const scoresRenderer = new ScoresRenderer();
-  SAMPLE_SCORES.forEach((score) => scoresRenderer.loadScore(score));
-  const NEW_SCORE_PULL = { name: 'Nacho', score: 200, };
+  scoresRenderer.addListener();
+  const NEW_SCORE_PULL = { name: 'Nacho', score: 200, }; // this is temporary input to populate the leaderboard
   scoresRenderer.loadScore(NEW_SCORE_PULL);
-
 }
